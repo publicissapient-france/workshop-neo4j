a1183130472e714e38c204c4bc47eb546acb8b5f
mbigorne <mathieu.bigorne@gmail.com>
2012-10-01 21:08:31 +0200

    fix test : '{variable}' doesn't work, use {variable} instead

--- src/main/java/fr/xebia/xke/neo4j/BiDAO.java
+++ src/main/java/fr/xebia/xke/neo4j/BiDAO.java
@@ -2,15 +2,10 @@ package fr.xebia.xke.neo4j;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import fr.xebia.xke.neo4j.domains.nodes.Product;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.index.Index;
-import org.neo4j.graphdb.index.IndexManager;
 import org.neo4j.helpers.collection.IteratorUtil;
 
 import java.util.Iterator;
@@ -21,22 +16,22 @@ public class BiDAO {
 
     protected GraphDatabaseService graphDb;
 
-    public BiDAO (GraphDatabaseService graphDb){
+    public BiDAO(GraphDatabaseService graphDb) {
         this.graphDb = graphDb;
     }
 
-    public List<String> getRecommendedProductsFor(String shoppingCartName){
+    public List<String> getRecommendedProductsFor(String shoppingCartName) {
         ExecutionEngine engine = new ExecutionEngine(graphDb);
         Map params = ImmutableMap.of("shoppingCartName", shoppingCartName);
-        ExecutionResult result = engine.execute("start sc=node:node_auto_index(name='{shoppingCartName}') " +
+        ExecutionResult result = engine.execute("start sc=node:node_auto_index(name={shoppingCartName}) " +
                 "MATCH sc-[:contient]->prod1<-[:contient]-otherSc-[:contient]->prod2 " +
                 "WHERE not (prod1 = prod2) AND not (sc-[:contient]->prod2) " +
-                "RETURN prod2.name", params);
+                "RETURN prod2", params);
 
         List<String> recommendedProducts = Lists.newArrayList();
-        Iterator<Node> n_column = result.columnAs( "n" );
-        for ( Node node : IteratorUtil.asIterable(n_column) ){
-            recommendedProducts.add((String)node.getProperty("name"));
+        Iterator<Node> n_column = result.columnAs("prod2");
+        for (Node node : IteratorUtil.asIterable(n_column)) {
+            recommendedProducts.add((String) node.getProperty("name"));
         }
         return recommendedProducts;
     }
