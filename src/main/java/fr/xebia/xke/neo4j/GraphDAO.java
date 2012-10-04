8fc2dba6ed827ec81c3c7918a59196e502f5269d
Mathieu Breton <mbreton@xebia.fr>
2012-10-04 13:01:42 +0200

    Suppression des implémentations

--- src/main/java/fr/xebia/xke/neo4j/GraphDAO.java
+++ src/main/java/fr/xebia/xke/neo4j/GraphDAO.java
@@ -29,25 +29,6 @@ public class GraphDAO {
      * @param sponsoredClientName Le nom du filleul
      */
     public void addNewSponsoredClient(String clientName, String sponsoredClientName) {
-        Transaction tx = graphDb.beginTx();
-        try {
-            ReadableIndex<Node> index = graphDb.index()
-                    .getNodeAutoIndexer()
-                    .getAutoIndex();
-
-            Node clientNode = index.get("name", clientName).getSingle();
-
-            Node sponsoredNode = graphDb.createNode();
-            sponsoredNode.setProperty("name", sponsoredClientName);
-
-            clientNode.createRelationshipTo(sponsoredNode, RelTypes.SPONSORED);
-
-            tx.success();
-        } catch (Exception e) {
-            tx.failure();
-        } finally {
-            tx.finish();
-        }
     }
 
     /**
@@ -55,19 +36,8 @@ public class GraphDAO {
      * @return La liste des noms de produit recommandés
      */
     public List<String> getRecommendedProductsFor(String productName) {
-        ExecutionEngine engine = new ExecutionEngine(graphDb);
-        Map params = ImmutableMap.of("productName", productName);
-        ExecutionResult result = engine.execute("start product=node:node_auto_index(name={productName}) " +
-                "MATCH product<-[:CONTAINS]-shoppingCart-[:CONTAINS]->recommendedProducts " +
-                "WHERE not (product = recommendedProducts) " +
-                "RETURN recommendedProducts", params);
-
-        List<String> recommendedProducts = Lists.newArrayList();
-        Iterator<Node> recommendedProductsColumn = result.columnAs("recommendedProducts");
-        for (Node node : IteratorUtil.asIterable(recommendedProductsColumn)) {
-            recommendedProducts.add((String) node.getProperty("name"));
-        }
-        return recommendedProducts;
+       List<String> recommendedProducts = Lists.newArrayList();
+       return recommendedProducts;
     }
 
     /**
@@ -78,19 +48,7 @@ public class GraphDAO {
      */
 
     public int getNumberOfSales(String productName, String color, Date date) {
-        ExecutionEngine engine = new ExecutionEngine(graphDb);
-        String formattedDate = "Date" + DateFormatUtils.format(date, "dd_MM_yyyy");
-        Map params = ImmutableMap.of("productName", productName,
-                "date", formattedDate,
-                "colorName", color);
-
-        ExecutionResult result = engine.execute("start date=node:node_auto_index(name={date}), " +
-                "product=node:node_auto_index(name={productName}), " +
-                "color=node:node_auto_index(name={colorName}) " +
-                "MATCH date<-[:DATE]-sc-[:CONTAINS]->product-[:COLOR]->color " +
-                "RETURN count(*)", params);
-
-        return Integer.parseInt(Iterables.getOnlyElement(result).get("count(*)").toString());
+        return 0;
     }
 
     /**
@@ -99,20 +57,7 @@ public class GraphDAO {
      */
     public List<String> getRecursiveSponsoredClient(String clientName) {
         ExecutionEngine engine = new ExecutionEngine(graphDb);
-        Map params = ImmutableMap.of("clientName", clientName);
-        ExecutionResult result = engine.execute("start client=node:node_auto_index(name={clientName}) RETURN client", params);
-        Node client = (Node) IteratorUtil.asIterable(result.columnAs("client")).iterator().next();
-
-        Traverser traverser = Traversal.description()
-                .depthFirst()//
-                .relationships(RelTypes.SPONSORED, Direction.OUTGOING)//
-                .evaluator(Evaluators.excludeStartPosition())//
-                .traverse(client);
-
         List<String> sponsored = Lists.newArrayList();
-        for (Path path : traverser) {
-            sponsored.add(path.endNode().getProperty("name").toString());
-        }
         return sponsored;
     }
 }
